3a772f22a6980535ec1fd95aa6608b20d053e50e
Sebastiao Silva <swillian2009@gmail.com>
2024-12-30 15:57:07 +0000

    Condicionais IF, ELSE e WHILE.

--- Main.java
+++ Main.java
@@ -4,26 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Insere o teu nome: ");
-        String nome = scanner.nextLine();
+        System.out.print("Por favor, informe-me a hora: ");
+        Double hora = scanner.nextDouble();
 
-        System.out.print("Insere o teu apelido: ");
-        String apelido = scanner.nextLine();
+        while(!(hora <= 23 && hora >= 0)){ 
+            System.out.println("Informe o horário, válido entre 00 e 23h");
+            hora = scanner.nextDouble();
+        }
 
-        System.out.println("Olá, " + nome + " " + apelido + ". Seja bem-vindo!");
-
-        System.out.print("Insere um número inteiro: ");
-        int primeiroNumero = scanner.nextInt();
-
-        System.out.print("Insere novamente outro número: ");
-        int segundoNumero = scanner.nextInt();
-
-        int soma = primeiroNumero + segundoNumero;
-        int multiplicacao = primeiroNumero * segundoNumero;
-
-        System.out.println(String.format("Resultado da soma %d + %d = %d", primeiroNumero, segundoNumero, soma));
-        System.out.println(String.format("Resultado da multiplicação %d * %d = %d", primeiroNumero, segundoNumero, multiplicacao));
-
+        if(hora >= 6 && hora <= 12){
+            System.out.println("Desejamos a si, um bom dia!");
+        }else if(hora >= 13 && hora <= 18){
+            System.out.println("Desejamos a si, uma boa tarde!");
+        }else{
+            System.out.println("Desejamos a si, uma boa noite!");
+        }
         scanner.close();
     }
 }
