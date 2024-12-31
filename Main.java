4393e3b626283b111ddd17a3a4d82496c3edf337
Sebastiao Silva <swillian2009@gmail.com>
2024-12-31 15:40:32 +0000

    :construction: Do WHILE.

--- Main.java
+++ Main.java
@@ -1,22 +1,23 @@
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int i, numeroEntrada, numeroLimite;
+        String palavraPasse = "Bingo", entrada;
+        int qtdTentativas = 0;
 
-        System.out.println("Por favor, informe um número para descobrirmos a tabuada dele!");
-        numeroEntrada = scanner.nextInt();
+        System.out.println("Por favor, digite a palavra-passe correta!");
         
-        System.out.println("Indique o tamanho do alcance da  tabuada.");
-        numeroLimite = scanner.nextInt();
-
-        for (i = 0; i <= numeroLimite; i++) {
-            int resultado = numeroEntrada * i;
-            System.out.println(String.format("A multiplicação de %d * %d = %d", numeroEntrada, i, resultado));
-        }
+        do {
+            System.out.println("Digite a palavra-passe corretamente!");
+            entrada = scanner.nextLine();
+            qtdTentativas++;
+        } while (!entrada.equals(palavraPasse));
+        
+        System.out.println(MessageFormat.format("Antes de inserir a palavra-passe corretamente o usuário tentou por {0} vezes.", (qtdTentativas -1)));
         System.out.println("Fim.");
-
+        
         scanner.close();
     }
 }
