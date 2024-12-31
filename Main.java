2cab4db9b9c3f3b4dbeadc5aadc738d7015fdf67
Sebastiao Silva <swillian2009@gmail.com>
2024-12-31 13:16:24 +0000

    :construction: Switch case.

--- Main.java
+++ Main.java
@@ -4,20 +4,73 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Por favor, informe-me a hora: ");
-        Double hora = scanner.nextDouble();
-
-        while(!(hora <= 23 && hora >= 0)){ 
-            System.out.println("Informe o horário, válido entre 00 e 23h");
-            hora = scanner.nextDouble();
-        }
-
-        if(hora >= 6 && hora <= 12){
-            System.out.println("Desejamos a si, um bom dia!");
-        }else if(hora >= 13 && hora <= 18){
-            System.out.println("Desejamos a si, uma boa tarde!");
-        }else{
-            System.out.println("Desejamos a si, uma boa noite!");
+        System.out.println("Por favor, informe-me o mês em questão, para sabermos quantos dias ele possui!");
+        int mes = scanner.nextInt();
+        String mesTrintaUm = "O mês possui 31 dias!";
+        String mesTrinta = "O mês possui 30 dias";
+        
+        switch (mes) {
+            case 1:
+                System.out.println("Janeiro, selecionado."); 
+                System.out.println(mesTrintaUm);
+                break;
+            case 2:
+            System.out.println("Fevereiro, selecionado. O ano é bissexto"); 
+            System.out.println("Digite o número 1, se a resposta for SIM!"); 
+            var entradaBissexto = scanner.nextInt();
+            switch (entradaBissexto) {
+                        case 1:
+                            System.out.println("O mês possui 29 dias!");
+                        break;
+                        default:
+                            System.out.println("O mês possui 28 dias!");
+                        break;
+                    }
+                    break;
+            case 3:
+                System.out.println("Março, selecionado.");  
+                System.out.println(mesTrintaUm);
+                break;
+            case 4:
+                System.out.println("Abril, selecionado.");  
+                System.out.println(mesTrinta);
+                break;
+            case 5: 
+                System.out.println("Maio, selecionado."); 
+                System.out.println(mesTrintaUm);
+                break;
+            case 6: 
+            System.out.println("Junho, selecionado."); 
+                System.out.println(mesTrinta);
+                break;
+            case 7: 
+            System.out.println("Julho, selecionado."); 
+                System.out.println(mesTrintaUm);
+                break;
+            case 8: 
+            System.out.println("Agosto, selecionado."); 
+                System.out.println(mesTrintaUm);
+                break;
+            case 9:
+            System.out.println("Setembro, selecionado.");  
+                System.out.println(mesTrinta);
+                break;
+            case 10: 
+            System.out.println("Outubro, selecionado."); 
+                System.out.println(mesTrintaUm);
+                break;
+            case 11: 
+            System.out.println("Novembro, selecionado."); 
+                System.out.println(mesTrinta);
+                break;
+            case 12: 
+            System.out.println("Dezembro, selecionado."); 
+                System.out.println(mesTrintaUm);
+                break;
+            
+            default:
+                System.out.println("Inserir números entre 1 e 12");
+            break;
         }
         scanner.close();
     }
