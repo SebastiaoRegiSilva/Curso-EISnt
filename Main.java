7b3a9de6af31125f7b6720d0b2de9241b7e8b84a
Sebastiao Silva <swillian2009@gmail.com>
2024-12-31 14:10:40 +0000

    :construction:Laço for.

--- Main.java
+++ Main.java
@@ -3,75 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i, numeroEntrada, numeroLimite;
 
-        System.out.println("Por favor, informe-me o mês em questão, para sabermos quantos dias ele possui!");
-        int mes = scanner.nextInt();
-        String mesTrintaUm = "O mês possui 31 dias!";
-        String mesTrinta = "O mês possui 30 dias";
+        System.out.println("Por favor, informe um número para descobrirmos a tabuada dele!");
+        numeroEntrada = scanner.nextInt();
         
-        switch (mes) {
-            case 1:
-                System.out.println("Janeiro, selecionado."); 
-                System.out.println(mesTrintaUm);
-                break;
-            case 2:
-            System.out.println("Fevereiro, selecionado. O ano é bissexto"); 
-            System.out.println("Digite o número 1, se a resposta for SIM!"); 
-            var entradaBissexto = scanner.nextInt();
-            switch (entradaBissexto) {
-                        case 1:
-                            System.out.println("O mês possui 29 dias!");
-                        break;
-                        default:
-                            System.out.println("O mês possui 28 dias!");
-                        break;
-                    }
-                    break;
-            case 3:
-                System.out.println("Março, selecionado.");  
-                System.out.println(mesTrintaUm);
-                break;
-            case 4:
-                System.out.println("Abril, selecionado.");  
-                System.out.println(mesTrinta);
-                break;
-            case 5: 
-                System.out.println("Maio, selecionado."); 
-                System.out.println(mesTrintaUm);
-                break;
-            case 6: 
-            System.out.println("Junho, selecionado."); 
-                System.out.println(mesTrinta);
-                break;
-            case 7: 
-            System.out.println("Julho, selecionado."); 
-                System.out.println(mesTrintaUm);
-                break;
-            case 8: 
-            System.out.println("Agosto, selecionado."); 
-                System.out.println(mesTrintaUm);
-                break;
-            case 9:
-            System.out.println("Setembro, selecionado.");  
-                System.out.println(mesTrinta);
-                break;
-            case 10: 
-            System.out.println("Outubro, selecionado."); 
-                System.out.println(mesTrintaUm);
-                break;
-            case 11: 
-            System.out.println("Novembro, selecionado."); 
-                System.out.println(mesTrinta);
-                break;
-            case 12: 
-            System.out.println("Dezembro, selecionado."); 
-                System.out.println(mesTrintaUm);
-                break;
-            
-            default:
-                System.out.println("Inserir números entre 1 e 12");
-            break;
+        System.out.println("Indique o tamanho do alcance da  tabuada.");
+        numeroLimite = scanner.nextInt();
+
+        for (i = 0; i <= numeroLimite; i++) {
+            int resultado = numeroEntrada * i;
+            System.out.println(String.format("A multiplicação de %d * %d = %d", numeroEntrada, i, resultado));
         }
+        System.out.println("Fim.");
+
         scanner.close();
     }
 }
